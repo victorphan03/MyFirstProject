0d928d0908839fbf12147befde56ca357fe8f259
Hien Phan <me@victorphan.net>
2024-03-16 08:14:54 +0700

    hoan thanh buoi thuc hanh 1

--- MyFirstRepo/app.js
+++ MyFirstRepo/app.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  
+  res.end("<h1>Bai Tap Nhom Cloud Computing and IoT</h1><br/><h3>Ly Thanh Hoang</h3><br><h3>Nguyen Thanh Thien</h3><br><h3>Duong Thanh Hoai<h3><br><h3>Phan Vo Dinh Hien</h3>");
 });
 
 server.listen(port,() => {
